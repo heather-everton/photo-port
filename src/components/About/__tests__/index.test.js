c9cc446f544d058b493846ea1f4369c7d38fe89a
Heather Everton <heather.everton@planplusonline.com>
2020-12-19 12:39:54 -0700

    update modal

--- src/components/About/__tests__/index.test.js
+++ src/components/About/__tests__/index.test.js
@@ -4,24 +4,16 @@ import '@testing-library/jest-dom/extend-expect';
 import About from '..';
 
 afterEach(cleanup);
-
-describe('About component', () => {
-    //renders About test
-})
-
   
-describe('About component', () => {
-    // First Test
+describe('About component renders', () => {
     it('renders', () => {
         render(<About />);
     });
 
-// Second Test
     it('matches snapshot DOM node structure', () => {
-        // render About
         const { asFragment } = render(<About />);
+
         expect(asFragment()).toMatchSnapshot();
-    })
-  
+    });
 })
   
